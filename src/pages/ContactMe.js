ac12d2d620f65b9b7011b5506f6aabf2a6f7497e
FizaWaseem <66438479+FizaWaseem@users.noreply.github.com>
2021-08-25 13:44:29 -0700

    update

--- src/pages/ContactMe.js
+++ src/pages/ContactMe.js
@@ -1,60 +1,69 @@
-import React from 'react'
-import {  Grid, Typography } from '@material-ui/core'
-import call from '../assests/call.png'
-import heart from '../assests/heart.png'
-import GitHubIcon from '@material-ui/icons/GitHub';
-import LinkedInIcon from '@material-ui/icons/LinkedIn';
-import MailIcon from '@material-ui/icons/Mail';
-import WhatsAppIcon from '@material-ui/icons/WhatsApp';
+import React from "react";
+import { Grid, Typography } from "@material-ui/core";
+import call from "../assests/call.png";
+import heart from "../assests/heart.png";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import MailIcon from "@material-ui/icons/Mail";
+import WhatsAppIcon from "@material-ui/icons/WhatsApp";
 export default function ContactMe() {
-    const Social=[
-        {id:1,
-        name:<GitHubIcon className="icon"/>,
-        link:"https://github.com/FizaWaseem",
-      
-        },
-         {id:2,
-        name: <LinkedInIcon className="icon"/>,
-        link:"https://www.linkedin.com/in/fiza-waseem-26749117a/",
-        },
-        {
-            id:3,
-            name:<MailIcon className="icon" />,
-            link:"mailto:wfiza842@gmail.com"
-        }
-        , {
-            id:4,
-            name:<WhatsAppIcon className="icon" />,
-            link:"https://wa.me/9203060622399 "
-        }
-    ];
-    const colors = ['rgb(0, 0, 0)', 'rgb(58, 143, 255)', 'rgb(252, 6, 6)','#53d365'];
-    return (
-        <div id="contact" className="conainer-fluid mt-5">
-        
-         <Grid xs={12} className="contact"  >
-         <Typography className="contactMe">
-         Contact Me <img src={call} alt="phone" className="victory"/>
+  const Social = [
+    {
+      id: 1,
+      name: <GitHubIcon className="icon" />,
+      link: "https://github.com/FizaWaseem",
+    },
+    {
+      id: 2,
+      name: <LinkedInIcon className="icon" />,
+      link: "https://www.linkedin.com/in/fiza-waseem-26749117a/",
+    },
+    {
+      id: 3,
+      name: <MailIcon className="icon" />,
+      link: "mailto:wfiza842@gmail.com",
+    },
+    {
+      id: 4,
+      name: <WhatsAppIcon className="icon" />,
+      link: "https://wa.me/9203060622399 ",
+    },
+  ];
+  const colors = [
+    "rgb(0, 0, 0)",
+    "rgb(58, 143, 255)",
+    "rgb(252, 6, 6)",
+    "#53d365",
+  ];
+  return (
+    <div id="contact" className="conainer-fluid mt-5">
+      <Grid xs={12} className="contact">
+        <Typography className="contactMe">
+          Contact Me <img src={call} alt="phone" className="victory" />
         </Typography>
         <Typography className="contactText">
-        DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY INBOX IS OPEN FOR ALL.
+          DISCUSS A PROJECT OR JUST WANT TO SAY HI? MY INBOX IS OPEN FOR ALL.
         </Typography>
-        <Grid style={{display:"flex"}}>
-      {Social.map((item,i)=>{
-          return(
-              <Grid key={item.id} className="social" style={{backgroundColor:colors[i],}} >
-            <a href={item.link} target="blank">
-              {item.name}</a>
+        <Grid style={{ display: "flex" }}>
+          {Social.map((item, i) => {
+            return (
+              <Grid
+                key={item.id}
+                className="social"
+                style={{ backgroundColor: colors[i] }}
+              >
+                <a href={item.link} target="blank">
+                  {item.name}
+                </a>
               </Grid>
-          )
-      })}
-     
+            );
+          })}
         </Grid>
         <Typography className="projectText">
-        Developed By <img src={heart} alt="heart" className="heart"/> by Fiza Waseem
+          Developed By <img src={heart} alt="heart" className="heart" /> by Fiza
+          Waseem
         </Typography>
-         </Grid>
-         
-        </div>
-    )
+      </Grid>
+    </div>
+  );
 }
--- src/pages/Progress.js
+++ src/pages/Progress.js
@@ -1,105 +1,109 @@
-import React from 'react'
-import fire from '../assests/fire.png'
-import { Grid, Typography } from '@material-ui/core'
-import Fade from 'react-reveal/Fade';
-import ProgressLine from '../components/ProgressBar';
- const Progress = () => {
-    return (
-        <Grid xs={12} className="process" >
-        <Fade bottom >
+import React from "react";
+import fire from "../assests/fire.png";
+import { Grid, Typography } from "@material-ui/core";
+import Fade from "react-reveal/Fade";
+import ProgressLine from "../components/ProgressBar";
+const Progress = () => {
+  return (
+    <Grid xs={12} className="process">
+      <Fade bottom>
         <Typography className="what">
-            Proficiency <img src={fire} alt="victory" className="victory"/>
+          Proficiency <img src={fire} alt="victory" className="victory" />
         </Typography>
-        </Fade>
-       
-        <Grid className="proRate">
+      </Fade>
+
+      <Grid className="proRate">
         <Grid xs={12} sm={6}>
-        <Fade bottom >
-                 <h3>
-                    
-                     <ProgressLine
-   label="React Js"
-        backgroundColor="#eedffc"
-        visualParts={[
-          {
-            percentage:"87%" ,
-            color: "#af74e6"
-          }
-        ]}
-      />
-     </h3>
-     <h3>
-                    
-                     <ProgressLine
-   label="Html/css"
-        backgroundColor="#eedffc"
-        visualParts={[
-          {
-            percentage:"96%" ,
-            color: "#af74e6"
-          }
-        ]}
-      />
-     </h3>
-     <h3>
-                    
-                    <ProgressLine
-  label="JavaScript"
-       backgroundColor="#eedffc"
-       visualParts={[
-         {
-           percentage:"78%" ,
-           color: "#af74e6"
-         }
-       ]}
-     />
-    </h3>
-    
-     <h3>
-                    
-                     <ProgressLine
-   label="React Native"
-        backgroundColor="#eedffc"
-        visualParts={[
-          {
-            percentage:"50%" ,
-            color: "#af74e6"
-          }
-        ]}
-      />
-     </h3>
-     <h3>
-                    
-                     <ProgressLine
-   label="MongDB"
-        backgroundColor="#eedffc"
-        visualParts={[
-          {
-            percentage:"60%" ,
-            color: "#af74e6"
-          }
-        ]}
-      />
-     </h3>
-     <h3>
-                    
-                     <ProgressLine
-   label="Node js"
-        backgroundColor="#eedffc"
-        visualParts={[
-          {
-            percentage:"65%" ,
-            color: "#af74e6"
-          }
-        ]}
-      />
-     </h3>
-     </Fade>
+          <Fade bottom>
+            <h3>
+              <ProgressLine
+                label="React Js"
+                backgroundColor="#eedffc"
+                visualParts={[
+                  {
+                    percentage: "95%",
+                    color: "#af74e6",
+                  },
+                ]}
+              />
+            </h3>
+            <h3>
+              <ProgressLine
+                label="Html/css"
+                backgroundColor="#eedffc"
+                visualParts={[
+                  {
+                    percentage: "96%",
+                    color: "#af74e6",
+                  },
+                ]}
+              />
+            </h3>
+            <h3>
+              <ProgressLine
+                label="JavaScript"
+                backgroundColor="#eedffc"
+                visualParts={[
+                  {
+                    percentage: "90%",
+                    color: "#af74e6",
+                  },
+                ]}
+              />
+            </h3>
+
+            <h3>
+              <ProgressLine
+                label="React Native"
+                backgroundColor="#eedffc"
+                visualParts={[
+                  {
+                    percentage: "70%",
+                    color: "#af74e6",
+                  },
+                ]}
+              />
+            </h3>
+            <h3>
+              <ProgressLine
+                label="MongDB"
+                backgroundColor="#eedffc"
+                visualParts={[
+                  {
+                    percentage: "60%",
+                    color: "#af74e6",
+                  },
+                ]}
+              />
+            </h3>
+            <h3>
+              <ProgressLine
+                label="Firebase"
+                backgroundColor="#eedffc"
+                visualParts={[
+                  {
+                    percentage: "50%",
+                    color: "#af74e6",
+                  },
+                ]}
+              />
+            </h3>
+            <h3>
+              <ProgressLine
+                label="Node js"
+                backgroundColor="#eedffc"
+                visualParts={[
+                  {
+                    percentage: "65%",
+                    color: "#af74e6",
+                  },
+                ]}
+              />
+            </h3>
+          </Fade>
         </Grid>
-      
-        </Grid>
-        
-        </Grid>  
-    )
-}
+      </Grid>
+    </Grid>
+  );
+};
 export default Progress;
